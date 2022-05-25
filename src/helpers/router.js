80657420471a1d9b687874d30c51dfd88e03e06d
Jason Watmore <cornflourblue@hotmail.com>
2022-05-25 16:51:09 +0930

    added index.js to views folder

--- src/helpers/router.js
+++ src/helpers/router.js
@@ -1,8 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import { useAuthStore } from '@/stores';
-import HomeView from '@/views/HomeView.vue';
-import LoginView from '@/views/LoginView.vue';
+import { HomeView, LoginView } from '@/views';
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/index.js
@@ -0,0 +1,2 @@
+export { default as HomeView } from './HomeView.vue';
+export { default as LoginView } from './LoginView.vue';
