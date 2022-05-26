23df36065fada58e65fc892ad6ecd5cef6b9e7ce
Jason Watmore <cornflourblue@hotmail.com>
2022-05-26 13:35:23 +0930

    added linkActiveClass: 'active' to router

--- src/helpers/router.js
+++ src/helpers/router.js
@@ -5,6 +5,7 @@ import { HomeView, LoginView } from '@/views';
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+    linkActiveClass: 'active',
     routes: [
         { path: '/', component: HomeView },
         { path: '/login', component: LoginView }
